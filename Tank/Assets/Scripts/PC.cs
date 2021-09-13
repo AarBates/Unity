fa704954692016cde31bfac685a37b697b7e19dc
Aaron <10880292@my.uvu.edu>
2021-09-12 19:34:37 -0600

    Finished development on Tank

---
+++ Tank/Assets/Scripts/PC.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PC : MonoBehaviour
+{
+    public float speed = 10.5f;
+    public float turnSpeed;
+    public float hInput;
+    public float vInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
+
+        transform.Rotate(Vector3.up, turnSpeed * hInput * Time.deltaTime);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * vInput);
+    }
+}
