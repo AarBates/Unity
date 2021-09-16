99c25b03f46374b4efde1c40ff7edea51512a7da
Aaron <10880292@my.uvu.edu>
2021-09-15 23:31:26 -0600

    Started the top-down project

---
+++ Top-Down/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 20.0f;
+    public float vInput;
+    public float hInput;
+    public float xRange = 12.4f;
+    public float yRange = 3.67f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.right * speed * hInput * Time.deltaTime);
+        transform.Translate(Vector3.up * speed * vInput * Time.deltaTime);
+
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.y < -yRange)
+        {
+            transform.position = new Vector3(transform.position.x, -yRange , transform.position.z);
+        }
+        if (transform.position.y > yRange)
+        {
+            transform.position = new Vector3(transform.position.x, yRange , transform.position.z);
+        }
+    }
+}
