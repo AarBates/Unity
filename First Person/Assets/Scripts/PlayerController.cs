5135b150de8693b3b867fad3885607ad5143072e
Aaron <10880292@my.uvu.edu>
2021-10-06 17:10:36 -0600

    Fixed player controller, basic jump mecahnic added

--- First Person/Assets/Scripts/PlayerController.cs
+++ First Person/Assets/Scripts/PlayerController.cs
@@ -24,18 +24,36 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Move();
+        camLook();
+        if(Input.GetButtonDown("Jump"))
+            jumpUp();
     }
 
     void Move()
     {
         float x = Input.GetAxis("Horizontal") * moveSpeed;
         float z = Input.GetAxis("Vertical") * moveSpeed;
-        RB.velocity = new Vector3(x, RB.velocity.y, z);
+        // RB.velocity = new Vector3(x, RB.velocity.y, z); - Old Code
+        Vector3 dir = transform.right * x + transform.forward * z;
+        RB.velocity = dir;
     }
 
     void camLook()
     {
         float y = Input.GetAxis("Mouse X") * lookSensetivity;
         rotX += Input.GetAxis("Mouse Y") * lookSensetivity;
+
+        rotX = Mathf.Clamp(rotX, minLookX, maxLookX);
+        chamera.transform.localRotation = Quaternion.Euler(-rotX, 0, 0);
+        transform.eulerAngles += Vector3.up * y;
+    }
+
+    void jumpUp()
+    {
+        Ray rayy = new Ray(transform.position, Vector3.down);
+        if(Physics.Raycast(rayy, 1.1f))
+        {
+            RB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
     }
 }
